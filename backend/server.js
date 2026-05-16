8132bbd4d82f10f7958628611397eed1997535e6
RITHVIKA19 <jakkularithvika2007@gmail.com>
2026-05-16 12:17:49 +0530

    updated code

--- backend/server.js
+++ backend/server.js
@@ -12,7 +12,7 @@ dotenv.config();
 const app=exp()
 app.use(
   cors({
-    origin: ["http://localhost:5173"],
+    origin: ["http://localhost:5173","https://blog-app-rose-delta.vercel.app/"],
     credentials:true  //enables to send the token back to clients
   })
 );
